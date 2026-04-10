0031f5441ce2fae1049a3670fa9d3e8038af4c8e
asafatzeypri <64274014+asafatzeypri@users.noreply.github.com>
2026-04-10 21:39:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// $("button").on("click" ,function() {
+//     $(".card").slideToggle();({opacity: 0.5});
+// });
+
+
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".burgerlinks");
+    const icon = document.querySelector(".burger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+
+
+}
+
